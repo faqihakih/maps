143f9aa047387ceadf8ae8addfe50128cf116d83
faqihakih <faqihakih12@gmail.com>
2021-05-22 04:22:32 +0700

    3 - done

--- app/src/main/java/com/akih/maps/MainActivity.kt
+++ app/src/main/java/com/akih/maps/MainActivity.kt
@@ -1,27 +1,28 @@
 package com.akih.maps
 
+import android.Manifest
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.akih.maps.databinding.ActivityMainBinding
+import com.github.florent37.runtimepermission.kotlin.askPermission
+import com.github.florent37.runtimepermission.kotlin.coroutines.experimental.askPermission
 import com.google.android.gms.dynamic.SupportFragmentWrapper
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MainActivity : AppCompatActivity(){
     private lateinit var binding: ActivityMainBinding
     private lateinit var mMaps: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        askPermission()
         val maps = MapsFragment()
-
-//        val mFragment = supportFragmentManager.findFragmentById(R.id.maps2) as SupportMapFragment
-//        mFragment.getMapAsync(this)
 
         binding.btnMaps.setOnClickListener {
             supportFragmentManager.beginTransaction().apply {
@@ -31,7 +32,35 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-//        mMaps = googleMap
+    private fun askPermission(){
+        askPermission(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ){
+
+        }.onDeclined{e ->
+            if (e.hasDenied()){
+                e.denied.forEach{
+
+                }
+
+                AlertDialog.Builder(this)
+                    .setMessage("Please Accept Our Permission")
+                    .setPositiveButton("Yes"){_,_ ->
+                        e.askAgain()
+                    }
+                    .setNegativeButton("No"){dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .show()
+            }
+
+            if (e.hasForeverDenied()){
+                e.foreverDenied.forEach {
+
+                }
+                e.goToSettings()
+            }
+        }
     }
 }
--- app/src/main/java/com/akih/maps/MapsFragment.kt
+++ app/src/main/java/com/akih/maps/MapsFragment.kt
@@ -1,6 +1,11 @@
 package com.akih.maps
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.util.AttributeSet
 import androidx.fragment.app.Fragment
@@ -8,7 +13,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.akih.maps.databinding.FragmentMapsBinding
+import com.github.florent37.runtimepermission.kotlin.askPermission
+import com.github.florent37.runtimepermission.kotlin.coroutines.experimental.askPermission
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
@@ -16,19 +26,44 @@ import com.google.android.gms.maps.model.MarkerOptions
 class MapsFragment : Fragment(), OnMapReadyCallback {
     private var _binding : FragmentMapsBinding? = null
     private val binding get() = _binding!!
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     private lateinit var mMaps: GoogleMap
+    var initial_marker    = "Seed nay"
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentMapsBinding.inflate(inflater, container, false)
         val mFragment = childFragmentManager.findFragmentById(R.id.maps2) as SupportMapFragment
         mFragment.getMapAsync(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        getLocation()
         return binding.root
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMaps = googleMap
-        Toast.makeText(this.context, "OnMapReady end", Toast.LENGTH_LONG).show()
+        if (ActivityCompat.checkSelfPermission(
+                        requireActivity(),
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        requireActivity(),
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        mMaps!!.setMyLocationEnabled(true)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation(){
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            location?.let { currentLocation ->
+                val sydney = LatLng(currentLocation.latitude, currentLocation.longitude);
+                mMaps.addMarker(MarkerOptions().position(sydney).title(initial_marker));
+                mMaps.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15f));
+            }
+        }
     }
 
     override fun onDestroyView() {
