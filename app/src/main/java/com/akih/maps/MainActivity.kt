dce3bc8ae9ea4bc79a795eb93a4fb7ebe7e1ab75
faqihakih <faqihakih12@gmail.com>
2021-05-21 23:05:44 +0700

    2 - aneh

--- app/src/main/java/com/akih/maps/MainActivity.kt
+++ app/src/main/java/com/akih/maps/MainActivity.kt
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.akih.maps.databinding.ActivityMainBinding
+import com.google.android.gms.dynamic.SupportFragmentWrapper
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mMaps: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -15,11 +20,18 @@ class MainActivity : AppCompatActivity() {
 
         val maps = MapsFragment()
 
+//        val mFragment = supportFragmentManager.findFragmentById(R.id.maps2) as SupportMapFragment
+//        mFragment.getMapAsync(this)
+
         binding.btnMaps.setOnClickListener {
             supportFragmentManager.beginTransaction().apply {
                 replace(R.id.container, maps)
                 commit()
             }
         }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+//        mMaps = googleMap
     }
 }
--- app/src/main/java/com/akih/maps/MapsFragment.kt
+++ app/src/main/java/com/akih/maps/MapsFragment.kt
@@ -1,14 +1,17 @@
 package com.akih.maps
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.akih.maps.databinding.FragmentMapsBinding
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsFragment : Fragment(), OnMapReadyCallback {
     private var _binding : FragmentMapsBinding? = null
@@ -16,22 +19,16 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var mMaps: GoogleMap
 
-    lateinit var mapView: MapView
-
-    private val MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey"
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentMapsBinding.inflate(inflater, container, false)
+        val mFragment = childFragmentManager.findFragmentById(R.id.maps2) as SupportMapFragment
+        mFragment.getMapAsync(this)
         return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.maps.getMapAsync(this@MapsFragment)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMaps = googleMap
+        Toast.makeText(this.context, "OnMapReady end", Toast.LENGTH_LONG).show()
     }
 
     override fun onDestroyView() {
